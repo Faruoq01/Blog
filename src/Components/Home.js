c21e28ca959630637419ce744b04e2df3edaaf5b
Faruoq01 <faruk.aminu@mabevic.com>
2022-01-09 18:34:24 +0100

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,9 +10,14 @@ import tiktok from '../assests/tiktok.png';
 import youtube from '../assests/youtube.png';
 import pinterest from '../assests/pinterest.png';
 import hero from '../assests/hero.jpeg';
+import book from '../assests/book.jpg';
+import code from '../assests/code.png';
+import coding from '../assests/coding.png';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
 const Home = () => {
@@ -68,28 +73,50 @@ const Home = () => {
                     {/* the hero starts here*/}
                     <Box mt={3} sx={{height:'100%', display:'flex', justifyContent:'space-between'}}>
                         <Box sx={{width:'70%', height:'100%', bgcolor:'green'}}>
-                            <Card sx={{ maxWidth: 345 }}>
+                            <Card sx={{height:'500px', padding:'10px'}}>
                                 <CardActionArea>
                                     <CardMedia
-                                    component="img"
-                                    height="140"
-                                    image="/static/images/cards/contemplative-reptile.jpg"
-                                    alt="green iguana"
+                                        component="img"
+                                        height="250"
+                                        image={book}
+                                        alt="green iguana"
+                                        sx={{
+                                            width:'100%',
+                                        }}
                                     />
                                     <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Lizard
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                                        species, ranging across all continents except Antarctica
-                                    </Typography>
+                                        <Typography gutterBottom sx={{fontWeight:'bold'}} variant="h6" component="div">
+                                            Useful Chrome Extensions that you shouldn't Miss Out
+                                        </Typography>
+                                        <Box sx={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                                            <Box sx={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                                                <img style={{width:'15px', height:'15px'}} alt={'icon'} src={code} />
+                                                <Typography sx={{marginLeft:'8px', fontSize:'12px', marginTop:'1px', color:'#000'}} variant="subtitle1" component="div">
+                                                    Development
+                                                </Typography>
+                                            </Box>
+                                            <Box sx={{marginLeft:'20px', display:'flex', flexDirection:'row', alignItems:'center'}}>
+                                                <img style={{width:'15px', height:'15px'}} alt={'icon'} src={coding} />
+                                                <Typography sx={{marginLeft:'8px', fontSize:'12px', marginTop:'1px', color:'#000'}} variant="subtitle1" component="div">
+                                                    Programming
+                                                </Typography>
+                                            </Box>
+                                        </Box>
+                                        <Typography mt={2} variant="body2" color="text.secondary">
+                                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                                            species, ranging across all continents except Antarctica Lizards are a 
+                                            widespread group of squamate reptiles, with over 6,000 species, ranging 
+                                            across all continents except Antarctica
+                                        </Typography>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
-                                    <Button size="small" color="primary">
-                                    Share
+                                    <Button sx={buttons} size="small" color="primary">
+                                        Continue Reading
                                     </Button>
+                                    <IconButton aria-label="share">
+                                        <ShareIcon />
+                                    </IconButton>
                                 </CardActions>
                             </Card>
                         </Box>
@@ -117,6 +144,19 @@ const social = {
     display:'flex',
     flexDirection:'row',
     justifyContent:'flex-end',
+}
+
+const buttons = {
+    bgcolor:'red',
+    height:'35px',
+    width:'150px', 
+    color:'#fff', 
+    fontSize:'11px', 
+    fontWeight:'bold',
+    '&:hover': {
+        backgroundColor: 'red',
+        color: '#fff'
+    },
 }
 
 export default Home;
