1c450b2010c98a7280a91248ca3b39d99c659cff
Faruoq01 <faruk.aminu@mabevic.com>
2022-01-20 00:57:26 +0100

    update

--- src/Components/management/Articles.js
+++ src/Components/management/Articles.js
@@ -1,14 +1,140 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import './styles.scss';
+import refresh from '../../assests/refresh.png';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TipTap from '../../tiptap/tiptap';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+  
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
 
 const Articles = () => {
+    const createArticle = () => {
+
+    }
     return(
         <React.Fragment>
-            <Box sx={{marginLeft:'85px'}}>
-                <h1>Articles</h1>
+            <Box sx={container}>
+                <Box sx={{width:'100%',display:'flex',flexDirection:'row', justifyContent:'flex-start'}}>
+                    <Typography sx={{fontWeight:'bold',marginLeft:'2%', marginTop:'20px', color:'#fff'}} variant="h5" component="div">
+                        Photography
+                    </Typography>
+                </Box>
+                <Box sx={section}>
+                    <Stack sx={stack} direction="row">
+                        <Typography sx={{fontWeight:'bold', color:'#000'}} variant="h6" component="div">
+                            List of articles
+                        </Typography>
+                        <Button sx={{fontSize:'12px'}} onClick={createArticle} variant="contained">New Article</Button>
+                    </Stack>
+                    <div id='table-caption2'>
+                        <div id='search-container'>
+                            <IconButton aria-label="share">
+                                <SearchIcon />
+                            </IconButton>
+                            <input type="text" placeholder={'Filter or search by name'} />
+                        </div>
+                        <button id='refresh-button'><img id='reload' src={refresh} alt='icon' /></button>
+                        <button id='refresh-button2'>Search</button>
+                    </div>
+                    <TipTap />
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 700, marginTop:'30px', marginBottom:'30px', }} aria-label="customized table">
+                            <TableHead>
+                            <TableRow>
+                                <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                                <StyledTableCell align="right">Calories</StyledTableCell>
+                                <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                                <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                                <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {rows.map((row) => (
+                                <StyledTableRow key={row.name}>
+                                <StyledTableCell component="th" scope="row">
+                                    {row.name}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                                <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                                <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                                <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                                </StyledTableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
             </Box>
         </React.Fragment>
     )
+}
+
+const container = {
+    width:"100%", 
+    marginLeft:'85px', 
+    display:'flex', 
+    flexDirection:'column', 
+    alignItems:'center'
+}
+
+const section = {
+    width:'96%',
+    marginTop:'30px',
+    borderRadius:'5px', 
+    bgcolor:'#fff',
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+}
+
+const stack = {
+    width:'96%',
+    display:'flex', 
+    justifyContent:'space-between', 
+    alignItems:'center',
+    marginTop:'20px'
 }
 
 export default Articles;
